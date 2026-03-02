43a64771c99d130a7f153feec38db1dccd559307
Yash Kollimarla <kyk32698@gmail.com>
2026-03-02 21:10:19 +0000

    AEVRA Website files

---
+++ script.js
@@ -0,0 +1,16 @@
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+threshold:0.2
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries){
+entries.forEach(entry=>{
+if(!entry.isIntersecting) return;
+entry.target.classList.add("show");
+});
+}, appearOptions);
+
+faders.forEach(fader=>{
+appearOnScroll.observe(fader);
+});
